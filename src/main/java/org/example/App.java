25170d523345119081ad6840d26b8be76d0b9fdb
averyreyna <avery.reyna16@gmail.com>
2021-09-13 22:14:56 -0400

    Updated code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -20,19 +20,19 @@ public class App
         // these if statements take into account both uppercase and lowercase inputs for temperature type
         if (conversion.equals("C") || conversion.equals("c"))
         {
-            Scanner input2 = new Scanner(System.in);
-            System.out.print("Please enter the temperature in Celcius: ");
-            int temperature = Integer.parseInt(input2.nextLine());
-            convertedTemperature = (temperature * 9 / 5) + 32;
-            System.out.println(String.format("Your temperature in Fahrenheit is %d.", convertedTemperature));
-        }
-        else if (conversion.equals("F") || conversion.equals("f"));
-        {
             Scanner input3 = new Scanner(System.in);
             System.out.print("Please enter the temperature in Fahrenheit: ");
             int temperature = Integer.parseInt(input3.nextLine());
             convertedTemperature = (temperature - 32) * 5 / 9;
             System.out.println(String.format("Your temperature in Celcius is %d.", convertedTemperature));
+        }
+        else if (conversion.equals("F") || conversion.equals("f"));
+        {
+            Scanner input2 = new Scanner(System.in);
+            System.out.print("Please enter the temperature in Celcius: ");
+            int temperature = Integer.parseInt(input2.nextLine());
+            convertedTemperature = (temperature * 9 / 5) + 32;
+            System.out.println(String.format("Your temperature in Fahrenheit is %d.", convertedTemperature));
         }
     }
 }
